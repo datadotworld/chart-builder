62963644a128ad1480eeb900556f11d341b2b3a1
DanialBetres <dbetres@uwaterloo.ca>
2019-09-13 16:09:14 -0500

    DATA-3022 Change hard-coded variables to env variables (#21)
    * DATA-3022 Change hard-coded variables to env variables
    
    * Add newline at end of env files

--- src/util/constants.js
+++ src/util/constants.js
@@ -3,5 +3,5 @@
 export const CLIENT_ID = String(process.env.REACT_APP_CLIENT_ID)
 export const CLIENT_SECRET = String(process.env.REACT_APP_CLIENT_SECRET)
 export const REDIRECT_URI = String(process.env.REACT_APP_REDIRECT_URI)
-export const API_HOST = 'https://api.data.world'
-export const OAUTH_HOST = 'https://data.world'
+export const API_HOST = String(process.env.REACT_APP_API_HOST)
+export const OAUTH_HOST = String(process.env.REACT_APP_OAUTH_HOST)
